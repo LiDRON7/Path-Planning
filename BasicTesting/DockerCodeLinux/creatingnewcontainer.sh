4fee95206e2f6a96acd4a37bc5d7118301e14b0d
Jediel-Robles <jediel.robles@upr.edu>
2026-03-02 21:09:47 -0400

    Added what I used for creating the container and running it

---
+++ BasicTesting/DockerCodeLinux/creatingnewcontainer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+xhost +local:docker
+docker run -it \
+  --name "px4dev" \
+  --env DISPLAY=$DISPLAY \
+  --env QT_X11_NO_MITSHM=1 \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  --device /dev/dri:/dev/dri \
+  --memory="16g" \
+  px4-gazebo
+
---
+++ BasicTesting/DockerCodeLinux/run_px4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xhost +local:docker
+docker start -ai px4dev
+
