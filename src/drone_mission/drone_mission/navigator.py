c7595651ac0993f27e241b9edd0b4f5daa1f532c
Jediel-Robles <jediel.robles@upr.edu>
2026-04-14 14:53:23 -0400

    Fixed code for hovering

--- src/drone_mission/drone_mission/navigator.py
+++ src/drone_mission/drone_mission/navigator.py
@@ -4,46 +4,56 @@ from px4_msgs.msg import TrajectorySetpoint
 
 class Navigator(BaseDrone):
     def __init__(self):
-        super().__init__('navigator_node') # node name
-
-        # Setup a Timer
-        # PX4 REQUIRES at least 2Hz of data to stay in Offboard mode.
+        super().__init__('navigator_node')
+        
         self.timer = self.create_timer(0.1, self.timer_callback)
 
-        #  Define target
-        #  NED coordinate system
         self.target_altitude = -5.0 
-        self.mission_started = False
+        self.counter = 0
 
     def timer_callback(self):
-        """runs every 0.1 seconds"""
-        
-        # Always send the Offboard Heartbeat
-        self.publish_offboard_control_mode()
 
-        # Send the  position 
+        self.publish_offboard_control_mode()
         self.publish_trajectory_setpoint()
 
-        # Attempt to Arm and Take Command (Once)
-        if not self.mission_started:
+
+        if self.counter < 20:
+            if self.counter == 0:
+                self.get_logger().info("Warm up")
+            self.counter += 1
+            return
+
+
+        if self.counter < 70:
+            if self.counter % 10 == 0:
+                self.get_logger().info(f"Takeoff (Attempt {self.counter//10})...")
+            
             self.arm()
             self.engage_offboard_mode()
-            self.mission_started = True
+            self.counter += 1
+        
+        else:
+            if self.counter == 70:
+                self.get_logger().info("Hover")
+            self.counter += 1
 
     def publish_trajectory_setpoint(self):
-        """Send drone"""
         msg = TrajectorySetpoint()
-        msg.position = [2.0, 2.0, self.target_altitude] # [X, Y, Z]
-        msg.yaw = 0.0 # Face North
+        msg.position = [0.0, 0.0, self.target_altitude]
+        msg.yaw = 0.0 
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.trajectory_setpoint_pub.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
     navigator = Navigator()
-    rclpy.spin(navigator) # Keep the node running
-    navigator.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(navigator)
+    except KeyboardInterrupt:
+        navigator.get_logger().info("User interrupted. Shutting down.")
+    finally:
+        navigator.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
